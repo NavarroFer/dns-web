e19c8182dae34f798ff0c34d68bbf10e5671f836
Fernando Navarro <f_e_r08@hotmail.com>
2020-12-03 19:47:59 -0300

    Saca comentarios

--- script.js
+++ script.js
@@ -3,24 +3,19 @@ var switchDesactDNSSECStatus = false;
 var switchdetallesDNSSECStatus = false;
 //script dns
 $('#inputDominio').keyup(function(event) {
-    // Number 13 is the "Enter" key on the keyboard
     if (event.keyCode === 13) {
-      // Cancel the default action, if needed
       event.preventDefault();
-      // Trigger the button element with a click
       document.getElementById("btnConsultar").click();
     }
   });
 
   $('#inputEDNS').keyup(function(event) {
-    // Number 13 is the "Enter" key on the keyboard
     if (event.keyCode === 13) {
-      // Cancel the default action, if needed
       event.preventDefault();
-      // Trigger the button element with a click
       document.getElementById("btnConsultar").click();
     }
   });
+  
 
   $('#btnConsultar').click("keyup", function() {
       consultarDNS();
